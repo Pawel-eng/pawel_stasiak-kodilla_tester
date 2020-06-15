e02401ae5d73d6379540882ded39ea722e840509
Pawel Stsiak <pawel_stasiak@poczta.onet.pl>
2020-06-15 16:36:35 +0200

    dodanie klasy LeapYear

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -2,7 +2,7 @@
 //    public static void main(String[] args) {
 //        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
 //        for (int i = 0, i < names.lenght; i++) {
-//            System.out.println(nams[i]);
+//            System.out.println(names[i]);
 //        }
 //
 //
