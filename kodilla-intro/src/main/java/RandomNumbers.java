1ca034d57a8bd269531cbc20fa65f9821adbb425
Pawel Stsiak <pawel_stasiak@poczta.onet.pl>
2020-08-15 17:54:18 +0200

    dodanie klasy Application.java

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,13 @@
+public class RandomNumbers {
+    public static void main(String[] args) {
+
+    }
+
+public void displayEvenNumbers(int min, int max) {
+    for (int i =0; i<31; i=i++) {
+        if (i + i > 5000) {
+        System.out.println(i);
+    }
+    }
+}
+}
