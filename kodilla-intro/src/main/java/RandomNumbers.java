c450661ed7b12949ed593640d7181f436a1ea465
Pawel Stsiak <pawel_stasiak@poczta.onet.pl>
2020-08-30 18:44:52 +0200

    dodanie klasy Application.java

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,13 +1,37 @@
+import java.util.Random;
+
 public class RandomNumbers {
+    
+    static int min = 10;
+    static int max = 0;
     public static void main(String[] args) {
-
+        displayEvenNumbers();
     }
 
-public void displayEvenNumbers(int min, int max) {
-    for (int i =0; i<31; i=i++) {
-        if (i + i > 5000) {
-        System.out.println(i);
+    public static void displayEvenNumbers() {
+        Random random = new Random();
+        int sum = 0;
+
+        while (sum < 5000) {
+            int temp = random.nextInt(31);
+            min(temp);
+            max(temp);
+            sum = sum + temp;
+            System.out.println(sum);
+        }
     }
+
+    public static void min(int number) {
+        if (number<min) {
+            min = number;
+            System.out.println("wartość minimalna "+min);
+        }
     }
-}
+
+    public static void max(int number) {
+        if (number>max) {
+            max = number;
+            System.out.println("wartość maxymalna "+max);
+        }
+    }
 }
